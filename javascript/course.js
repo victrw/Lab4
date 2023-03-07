6ceea329650375a4e705e9ec9e2db36ea016423d
Victor Wong <test@gmail.com>
2023-03-06 20:01:58 -0800

    Created course array in course.js

---
+++ javascript/course.js
@@ -0,0 +1,12 @@
+const courselist = [
+    {code: "ACIT 1620",
+    name: "Web Fundamental Technologies"
+}, {
+    code: "ACIT 1630",
+    name: "Database Systems"
+}, {
+    code: "Math 1310",
+    name: "Technical Math for IT"
+}
+
+]
