e71a292edc79eaffb9a1dd3831f4e7c229883280
Victor Wong <test@gmail.com>
2023-03-06 23:04:48 -0800

    Added prompt user again for invaild data

--- javascript/course.js
+++ javascript/course.js
@@ -10,3 +10,32 @@ const courselist = [
 }
 
 ]
+
+
+// ----- step 1: get 4-digit number from user -------
+
+let userinput
+// userinput = prompt("Please enter a 4 digit course code. (e.g. '1310') ");
+
+// ----- step 2: prompt user again if invalid data is entered --------
+
+do {
+    userinput = prompt("Please enter a 4 digit course code. (e.g. '1310') ");
+    if (userinput.length !== 4 || isNaN(userinput)) {
+        alert("Invalid data, please try again!");
+    }
+} while (userinput.length !== 4 || isNaN(userinput));
+
+// ------ step 3: write a loop to iterate through courselist array -------
+
+if (courselist.indexOf(userinput) = -1) {
+    courselist.push(
+        {code: userinput,
+        name: "null"}
+    );
+    console.log("Course code has been successfully added to courselist.")
+    console.log(courselist)
+} else {
+        console.log(`Yes, I am taking the course: ${courselist["code"]} - ${courselist["name"]}`);
+    }
+
